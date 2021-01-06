22150fd583ec8d40c604c05fdb03680d53e928df
OsOmE1 <osome1@protonmail.com>
2021-01-06 20:06:45 +0100

    Added Beebyte-Deobfuscator

---
+++ Loaders/Beebyte-Deobfusctator/Deobfuscator/ApkDeobfuscator.cs
@@ -0,0 +1,34 @@
+﻿using Beebyte_Deobfuscator.Lookup;
+using Il2CppInspector.PluginAPI;
+using Il2CppInspector.Reflection;
+using System.Collections.Generic;
+
+namespace Beebyte_Deobfuscator.Deobfuscator
+{
+    public class ApkDeobfuscator : IDeobfuscator
+    {
+        public ApkDeobfuscator()
+        {
+
+        }
+        public LookupModel Process(TypeModel model, BeebyteDeobfuscatorPlugin plugin)
+        {
+            PluginServices services = PluginServices.For(plugin);
+            if (!(model.Package.BinaryImage is Il2CppInspector.APKReader)) throw new System.ArgumentException("APK deobfuscation can only be used with obfuscated APKs");
+
+            services.StatusUpdate("Loading unobfuscated APK");
+            List<string> packages = new List<string> { plugin.ApkPath.Value };
+            var il2cppClean = Il2CppInspector.Il2CppInspector.LoadFromPackage(packages, statusCallback: services.StatusUpdate);
+
+            services.StatusUpdate("Creating type model for unobfuscated APK");
+            var modelClean = new TypeModel(il2cppClean[0]);
+
+            services.StatusUpdate("Creating LookupModel for obfuscated APK");
+            LookupModel lookupModel = new LookupModel(modelClean, modelClean, plugin.NamingRegex.Value);
+            services.StatusUpdate("Deobfuscating binary");
+            lookupModel.TranslateTypes();
+
+            return lookupModel;
+        }
+    }
+}
---
+++ Loaders/Beebyte-Deobfusctator/Lookup/LookupMatrix.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Beebyte_Deobfuscator.Lookup
+{
+    struct LookupVertex
+    {
+        public LookupType Type { get; set; }
+        public int StaticFieldCount { get; set; }
+        public int LiteralFieldCount { get; set; }
+        public int GenericFieldCount { get; set; }
+        public int PropertyCount { get; set; }
+
+    }
+    class LookupMatrix
+    {
+        private HashSet<LookupVertex>[,,,] _matrix;
+        public LookupMatrix(int x, int y, int z, int w)
+        {
+            _matrix = new HashSet<LookupVertex>[x * 2 + 1, y * 2 + 1, z * 2 + 1, w * 2 + 1];
+        }
+
+        public void Insert(LookupVertex vertex)
+        {
+            if (_matrix[vertex.StaticFieldCount, vertex.LiteralFieldCount, vertex.GenericFieldCount, vertex.PropertyCount] == null)
+            {
+                _matrix[vertex.StaticFieldCount, vertex.LiteralFieldCount, vertex.GenericFieldCount, vertex.PropertyCount] = new HashSet<LookupVertex>() { vertex };
+            }
+            else
+            {
+                _matrix[vertex.StaticFieldCount, vertex.LiteralFieldCount, vertex.GenericFieldCount, vertex.PropertyCount].Add(vertex);
+            }
+        }
+
+        public void Insert(LookupType item)
+        {
+            Insert(new LookupVertex() { Type = item, StaticFieldCount = item.Fields.Count(f => f.IsStatic), LiteralFieldCount = item.Fields.Count(f => f.IsLiteral), GenericFieldCount = item.Fields.Count(f => !f.IsStatic && !f.IsLiteral), PropertyCount = item.Properties.Count });
+        }
+
+        public HashSet<LookupVertex> GetVertices(LookupVertex vertex)
+        {
+            return _matrix[vertex.StaticFieldCount, vertex.LiteralFieldCount, vertex.GenericFieldCount, vertex.PropertyCount];
+        }
+
+        public List<LookupType> Get(LookupType item)
+        {
+            HashSet<LookupVertex> vertexSet = GetVertices(new LookupVertex() { Type = item, StaticFieldCount = item.Fields.Count(f => f.IsStatic), LiteralFieldCount = item.Fields.Count(f => f.IsLiteral), GenericFieldCount = item.Fields.Count(f => !f.IsStatic && !f.IsLiteral), PropertyCount = item.Properties.Count });
+            List<LookupType> typeList = new List<LookupType>();
+            if (vertexSet == null || vertexSet.Count == 0) return typeList;
+
+            typeList.AddRange(vertexSet.Select(x => x.Type));
+            return typeList;
+        }
+    }
+}
